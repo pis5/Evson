27dc7fa18a1c237274eeea680d3eab13f435ba9d
kassraoui <mehdikassraoui@gmail.com>
2016-12-01 22:24:51 +0000

    register + login terminé

--- app/src/main/java/com/example/mkass/evson/signin.java
+++ app/src/main/java/com/example/mkass/evson/signin.java
@@ -5,7 +5,21 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import customfonts.MyEditText;
+import customfonts.MyTextView;
+import entities.Personne;
 
 public class signin extends AppCompatActivity {
 
@@ -13,6 +27,12 @@ public class signin extends AppCompatActivity {
 
     Typeface fonts1;
 
+    MyEditText emailET;
+    MyEditText passwordET;
+    MyTextView errorMsgET;
+    MyTextView btnSignIn;
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +62,112 @@ public class signin extends AppCompatActivity {
         TextView t4 =(TextView)findViewById(R.id.create);
         t4.setTypeface(fonts1);
 
+        emailET = (MyEditText)findViewById(R.id.email);
+        passwordET = (MyEditText)findViewById(R.id.password);
+        errorMsgET = (MyTextView)findViewById(R.id.errorMsg);
+        btnSignIn = (MyTextView)findViewById(R.id.signin1);
+        progressBar = (ProgressBar)findViewById(R.id.progress);
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
 
+    }
+
+    /**
+     * Method gets triggered when Login button is clicked
+     *
+     * @param view
+     */
+    public void loginUser(View view){
+        // Get Email Edit View Value
+        String email = emailET.getText().toString();
+        // Get Password Edit View Value
+        String password = passwordET.getText().toString();
+        // Instantiate Http Request Param Object
+        RequestParams params = new RequestParams();
+        // When Email Edit View and Password Edit View have values other than Null
+        if(Utility.isNotNull(email) && Utility.isNotNull(password)){
+            // When Email entered is Valid
+            if(Utility.validate(email)){
+                // Put Http parameter username with value of Email Edit View control
+                params.put("email", email);
+                // Put Http parameter password with value of Password Edit Value control
+                params.put("motDePasse", password);
+                // Invoke RESTful Web Service with Http parameters
+                invokeWS(params);
+            }
+            // When Email is invalid
+            else{
+                Toast.makeText(getApplicationContext(), "Please enter valid email", Toast.LENGTH_LONG).show();
+            }
+        } else{
+            Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    /**
+     * Method that performs RESTful webservice invocations
+     *
+     * @param params
+     */
+    public void invokeWS(RequestParams params){
+        // Show Progress Dialog
+        progressBar.setVisibility(View.VISIBLE);
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(R.string.ipAdress + "/useraccount/login/dologin",params ,new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                // Hide Progress Dialog
+                progressBar.setVisibility(View.GONE);
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if(obj.getBoolean("status")){
+                        Toast.makeText(getApplicationContext(), "You are successfully logged in!", Toast.LENGTH_LONG).show();
+                        // Navigate to Home screen
+
+                    }
+                    // Else display error message
+                    else{
+                        errorMsgET.setText(obj.getString("error_msg"));
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+            // When the response returned by REST has Http response code other than '200'
+
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                progressBar.setVisibility(View.GONE);
+                // When Http response code is '404'
+                if(statusCode == 404){
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if(statusCode == 500){
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else{
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
